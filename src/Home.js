565c60ede2db962df73c2029149f40b882dc0ff3
luisaGonzales <luisagc711@gmail.com>
2017-10-10 12:07:48 -0500

    Utils , avance de home

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,21 @@ import {
 	NavLink,
 	Redirect
 } from 'react-router-dom'
+import './Home.css'
+
 
 const Home = ( {model} ) => {
 
 	return (
-		<div>
+		<div className="container">
+			<div className="row">
+				<div className="col-xs-12">
+					<img src="http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c525.png" className="logo"/>
+				</div>
+				
+			</div>		
+			
+
 			<NavLink to={"/signup"}
 			         className="btn btn-lyft btn-lg btn-block"
 			>
