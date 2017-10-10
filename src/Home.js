d2d80ad3dc41f9879954e82bfe00ccb34320f3d1
luisaGonzales <luisagc711@gmail.com>
2017-10-10 14:23:11 -0500

    Home terminado

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
 import {
-	BrowserRouter,
-	Route,
-	Switch,
-	NavLink,
-	Redirect
+	NavLink
 } from 'react-router-dom'
 import './Home.css'
 
@@ -14,16 +10,28 @@ const Home = ( {model} ) => {
 	return (
 		<div className="home container">
 			<div className="row">
-				<div className="col-xs-12">
+				<div className="col-xs-offset-3 col-xs-6">
 					<img src="http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c525.png" className="logo"/>
 				</div>
-				
-			</div>		
-			<NavLink to={"/signup"}
-			         className="btn btn-lyft btn-lg btn-block btn-signUp"
-			>
-				Sign Up
-			</NavLink>
+			</div>	
+			<div className="row  btns">
+				<div className="col-xs-6 col-lg-6 col-md-6">
+					<button to={"/signup"}
+							className="btn btn-lyft btn-lg btn-block btn-signUp"
+							disabled
+					>
+						Log In
+					</button>
+				</div>
+				<div className="col-xs-6 col-lg-6 col-md-6">
+					<NavLink to={"/signup"}
+							className="btn btn-lyft btn-lg btn-block btn-signUp"
+					>
+						Sign Up
+					</NavLink>
+				</div>
+			</div>	
+			
 		</div>
 	);
 }
