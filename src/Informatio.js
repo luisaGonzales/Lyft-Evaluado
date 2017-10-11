7f458a36d4a42289679d26bae9d38475553efcab
DianaQuispe <danaalienmas@gmail.com>
2017-10-11 14:26:49 -0500

    agregado de flecha

--- src/Informatio.js
+++ src/Informatio.js
@@ -1,32 +1,34 @@
-import React, {Component} from 'react';
-import GoogleMaps from './GoogleMaps';
-import ReactGoogleAutocomplete from './ReactGoogleAutocomplete';
-import './LyftMap.css'
+import React, { Component } from "react";
+import GoogleMaps from "./GoogleMaps";
+import ReactGoogleAutocomplete from "./ReactGoogleAutocomplete";
+import "./LyftMap.css";
 import {
-	BrowserRouter,
-	Route,
-	Switch,
-	NavLink,
-	Redirect
-} from 'react-router-dom'
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink,
+  Redirect
+} from "react-router-dom";
 
 class Info extends Component {
-	constructor(props) {
-		super(props)
-	}
-	render() {
-		const { model } = this.props;		
-		return(	
-			<div>
-				<h2>LyftMap </h2>
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { model } = this.props;
+    return (
+      <div>
+         <NavLink to="/lyftmap">
+                <i className="material-icons back">keyboard_arrow_left</i>
+              </NavLink>
+        <h2>LyftMap </h2>
 
-				<p>NOMBRE: {model.userInfo.name}</p>
-				<p>EMAIL:  {model.userInfo.email}</p>
-				<p>PHONE:  {model.userInfo.phone}</p>
+        <p>NOMBRE: {model.userInfo.name}</p>
+        <p>EMAIL: {model.userInfo.email}</p>
+        <p>PHONE: {model.userInfo.phone}</p>
 
-			</div>
-		)
-		
-	}
+      </div>
+    );
+  }
 }
 export default Info;
